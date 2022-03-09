81da620d5ac29097fea329ca6a40cf7667fc0505
HartMason <hartmason85@gmail.com>
2022-03-08 22:18:18 -0600

    last commit

--- controller/users.js
+++ controller/users.js
@@ -4,29 +4,55 @@ const sampleUser = require("../data/sampleUser")
 
 const listUsers = (req, res) => {
     res.json(users) 
+    //server timed out is a server issue 500
 };
 
 const showUser = (req, res) => {
     const contactID = users.find(data => data.id == req.params.id)            //find someone by id
     console.log(contactID)
+    if (!contactID) {
+        res.status(404).json({msg : `User id ${req.params.id} not found`}) 
+    } else {
     res.send(contactID)
-}
+    }
+    //404 
+    //if/else 
+    //if the response came back good or not
+};
 
 const createUser = (req, res) => {
-    users.push(sampleUser)
-    res.json(users)
-}
+    if(!newUsers.id) {
+        res.status(400).json({ msg: "New users require an id" })
+    } else {
+        users.push(sampleUser)
+        res.json(users)
+    }
+};
 
 const updateUser = (req, res) => {
     const contactID = users.find(data => data.id == req.params.id)
-    console.log(contactID)
-    contactID.name = "Joseph"
-}
+    // console.log(contactID)
+    if (contactID){
+        contactID.name = "Joseph";
+        res.json(contactID)
+    } else {
+        res.status(404).json({msg : `User id ${req.params.id} not found`}) //req.params.id. is trying grab the id
+    }
+};
+
 
 const deleteUser = (req, res) => {
-    const contactID = users.find(data => data.id == req.params.id)
-    users.filter(user => user.id !== parseInt(req.params.id))          
-}
+    const found = users.some(user => user.id === parseInt(req.params.id))
+        if (found){
+            res.json({
+                msg: 'user deleted',
+                users: users.filter(user => user.id !== parseInt(req.params.id))
+            }) 
+        } else {
+            res.status(404).json({msg : `User id ${req.params.id} not found`})
+            }
+    };          
 
 
-module.exports = {listUSers, showUser, createUser, deleteUser}             
+
+module.exports = {listUsers, showUser, createUser, updateUser, deleteUser}             
--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,15 @@ const express = require('express')
 const router = express.Router()
 const usersController = require("../controller/users")    //bringing the logic from users.js controller file in. 
 // * GET /users  * Return all users
-router.get("/users", usersController.list) 
+router.get("/users", usersController.listUsers) 
 
-router.get("/users/:id", usersController.list) 
+router.get("/users/:id", usersController.showUser) 
 
-router.post("/users", usersController.list) 
+router.post("/users", usersController.createUser) 
 
-router.put("/users/:id", usersController.list) 
+router.put("/users/:id", usersController.updateUser) 
 
-router.delete("/users/:id", usersController.list) 
-
+router.delete("/users/:id", usersController.deleteUser) 
 
 
 
